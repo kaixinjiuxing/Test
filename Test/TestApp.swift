51ab3a615ba11583b5ff86689bbf20de4e9acafe
kaixinjiuxing <hanaruihanliu@gmail.com>
2023-06-26 10:32:57 -0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Ruihan Liu on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
